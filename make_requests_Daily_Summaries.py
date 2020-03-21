ba9afa3e17fc0b9bdc44a6620eb62b842461136c
Norton Li <nli@zipcodes-MBP.home>
2020-03-21 19:57:10 -0400

    Lab completed, all analysis done

--- make_requests_Daily_Summaries.py
+++ make_requests_Daily_Summaries.py
@@ -7,7 +7,7 @@ file_counter = 0
 offset_counter = 1
 token = 'vgExQgdFRrflyczUiJQVHBXBqzMwqpXz'
 
-while file_counter <= 3:
+while file_counter <= 1:
     name = ("daily_summaries_FIPS10003_jan_2018_" + str(file_counter))
     url = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=\
     GHCND&locationid=FIPS:10003&startdate=2018-01-01&enddate=2018-01-31&limit=1000&offset=' \
